fc040c2c268ec34385650c2a3acfba43f4b55e7f
Jordan Brandes <jordanbrandes@yahoo.com>
2019-06-28 13:33:03 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+def recipe():
+    title = input("Great! What would you like to call your recipe? ")
+    ingredients = input("What ingredients are in your recipe? ")
+    step1 = input("What do we do first in order to prepare the dish? ")
+    step2 = input("What comes next? ")
+    step3 = input("And after that? ")
+    step4 = input("How do we know when the reciple is finished? ")
+    step5 = input("What is the best way to serve the dish? ")
+    print(title + '\n', ingredients + '\n', step1 + '\n', step2 + '\n', step3 + '\n', step4 + '\n', step5 + 'n')
+    
+print("Welcome to recipe maker. Would you like to start a new recipe? ")
+answer = input()
+
+if answer == "yes":
+  recipe()
+else:
+    print("That's too bad, maybe next time. Thanks for visiting.")
