8d5dacabcb587ade5ba76879a1d776c442744807
Jordan Brandes <jordanbrandes@yahoo.com>
2019-06-28 14:47:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sys
+
+f = open('recipe.txt', 'w')
+
 def recipe():
     title = input("Great! What would you like to call your recipe? ")
     ingredients = input("What ingredients are in your recipe? ")
@@ -6,8 +10,11 @@ def recipe():
     step3 = input("And after that? ")
     step4 = input("How do we know when the reciple is finished? ")
     step5 = input("What is the best way to serve the dish? ")
-    print(title + '\n', ingredients + '\n', step1 + '\n', step2 + '\n', step3 + '\n', step4 + '\n', step5 + 'n')
+
+    print(title + '\n', ingredients + '\n', step1 + '\n', step2 + '\n', step3 + '\n', step4 + '\n', step5 + 'n', file = f)
     
+
+
 print("Welcome to recipe maker. Would you like to start a new recipe? ")
 answer = input()
 
@@ -15,3 +22,4 @@ if answer == "yes":
   recipe()
 else:
     print("That's too bad, maybe next time. Thanks for visiting.")
+
